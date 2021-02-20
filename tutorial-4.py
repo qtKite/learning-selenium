9ef72eb18ccbce81609ef48479617cd86b11df9b
qtKite <jackywen4@gmail.com>
2021-02-20 13:02:56 +1100

    connecting to the cookie clicker

--- tutorial-4.py
+++ tutorial-4.py
@@ -4,9 +4,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 PATH = "C:/jxcky/bin/web-drivers/chromedriver.exe"
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path=PATH, options=options)
-driver.get("https://techwithtim.net")
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+
+cookie = driver.find_element_by_id("bigCookie")
+cookie_count = driver.find_element_by_id("cookies")
+
+actions = ActionChains(driver)
+actions.click() # we can queue a bunch of actions then perform them
+actions.perform()
