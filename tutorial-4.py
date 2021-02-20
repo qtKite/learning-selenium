25af137eefe0a5884c076a7426cc121729aad68c
qtKite <jackywen4@gmail.com>
2021-02-20 13:13:55 +1100

    cookie clicker

--- tutorial-4.py
+++ tutorial-4.py
@@ -12,9 +12,25 @@ options.add_experimental_option('excludeSwitches', ['enable-logging'])
 driver = webdriver.Chrome(executable_path=PATH, options=options)
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-cookie = driver.find_element_by_id("bigCookie")
+# wait 5 seconds so page loads
+driver.implicitly_wait(3)
+
+cookieButton = driver.find_element_by_id("bigCookie")
 cookie_count = driver.find_element_by_id("cookies")
+items = [driver.find_element_by_id("productPrice" + str(i)) for i in range(1, -1, 1)]
 
 actions = ActionChains(driver)
-actions.click() # we can queue a bunch of actions then perform them
-actions.perform()
+actions.click(cookieButton) # we can queue a bunch of actions then perform them
+
+for i in range(5000):
+    actions.perform()
+    count = cookie_count.text.split(" ")[0]
+    print("cookies: " + count)
+
+    for item in items:
+        value = int(item.text)
+        if value <= count:
+            upgrade_actions = ActionChains(driver)
+            upgrade_actions.move_to_element(item)
+            upgrade_actions.click()
+            upgrade_action.perform()
